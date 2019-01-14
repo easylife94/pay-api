550e855ab8edb06c5a1acf4b9975b68f399b6636
chenwei <cw961590280@hotmail.com>
2019-01-14 09:59:26 +0800

    x

---
+++ pay-api-web/src/main/java/com/pay/api/web/PayApiWebApplication.java
@@ -0,0 +1,14 @@
+package com.pay.api.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayApiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApiWebApplication.class, args);
+    }
+
+}
+
---
+++ pay-api-web/src/test/java/com/pay/api/web/PayApiWebApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pay.api.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PayApiWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
