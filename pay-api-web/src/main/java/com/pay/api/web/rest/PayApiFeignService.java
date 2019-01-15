2872fee7445fd3363d5b7d0bd539d9df64406f73
chenwei <cw961590280@hotmail.com>
2019-01-15 14:34:34 +0800

    x

---
+++ pay-api-web/src/main/java/com/pay/api/web/rest/PayApiFeignService.java
@@ -0,0 +1,23 @@
+package com.pay.api.web.rest;
+
+import com.pay.api.client.service.IPayApiFeignService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * RESTfull服务实现
+ *
+ * @author chenwei
+ * @date 2019/1/14 14:15
+ */
+@RestController
+public class PayApiFeignService implements IPayApiFeignService {
+
+    private static Logger logger = LoggerFactory.getLogger(PayApiFeignService.class);
+
+    @Override
+    public String test() {
+        return "service is available";
+    }
+}
