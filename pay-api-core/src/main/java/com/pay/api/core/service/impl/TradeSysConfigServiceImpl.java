971a70a7032d7168169b80d92cf61e3437ad27d9
chenwei <chenwei@qq.com>
2019-03-12 20:43:13 +0800

    实现获取交易系统配置服务接口

--- pay-api-core/src/main/java/com/pay/api/core/service/impl/TradeSysConfigServiceImpl.java
+++ pay-api-core/src/main/java/com/pay/api/core/service/impl/TradeSysConfigServiceImpl.java
@@ -1,7 +1,10 @@
 package com.pay.api.core.service.impl;
 
 import com.pay.api.client.dto.TradeSysConfigDTO;
+import com.pay.api.client.model.TradeSysConfigDO;
+import com.pay.api.core.dao.TradeSysConfigDao;
 import com.pay.api.core.service.ITradeSysConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,9 +14,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class TradeSysConfigServiceImpl implements ITradeSysConfigService {
 
+    private final TradeSysConfigDao tradeSysConfigDao;
+
+    @Autowired
+    public TradeSysConfigServiceImpl(TradeSysConfigDao tradeSysConfigDao) {
+        this.tradeSysConfigDao = tradeSysConfigDao;
+    }
+
     @Override
     public TradeSysConfigDTO getConfig(String configKey) {
-        //todo 等待新增model和dao
-        return null;
+        TradeSysConfigDTO tradeSysConfigDTO = null;
+        TradeSysConfigDO tradeSysConfigDO = tradeSysConfigDao.selectOneByConfigKey(configKey);
+        if (tradeSysConfigDO != null) {
+            tradeSysConfigDTO = new TradeSysConfigDTO(tradeSysConfigDO.getConfigKey(), tradeSysConfigDO.getConfigValue());
+        }
+        return tradeSysConfigDTO;
     }
 }
