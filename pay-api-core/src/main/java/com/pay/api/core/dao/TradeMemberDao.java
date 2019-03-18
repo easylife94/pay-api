ba9363887fcf3685c87730aabd5cfe645829a1ea
chenwei <chenwei@qq.com>
2019-03-18 21:54:24 +0800

    下单接口优化：会员订单号是否存在使用count语句，订单记录一次保存，会员查询添加缓存

--- pay-api-core/src/main/java/com/pay/api/core/dao/TradeMemberDao.java
+++ pay-api-core/src/main/java/com/pay/api/core/dao/TradeMemberDao.java
@@ -1,8 +1,11 @@
 package com.pay.api.core.dao;
 
 import com.pay.api.client.model.TradeMemberDO;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 
 public interface TradeMemberDao {
+
     int deleteByPrimaryKey(Long id);
 
     int insert(TradeMemberDO record);
@@ -11,9 +14,12 @@ public interface TradeMemberDao {
 
     TradeMemberDO selectByPrimaryKey(Long id);
 
+    @CacheEvict(value = "tradeMemberCache", allEntries=true)
     int updateByPrimaryKeySelective(TradeMemberDO record);
 
+    @CacheEvict(value = "tradeMemberCache", allEntries=true)
     int updateByPrimaryKey(TradeMemberDO record);
 
+    @Cacheable(value = "tradeMemberCache", key = "'TradeMemberDO:memberNumber:'.concat(#root.args[0])")
     TradeMemberDO selectByMemberNumber(String memberNumber);
 }
