b8cf6145991d41c634cacb492e97f629b8bae901
chenwei <cw961590280@hotmail.com>
2019-03-26 18:41:01 +0800

    1.新增发送数据统计消息2.移除无用实体和相关dao配置文件

--- pay-api-core/src/main/java/com/pay/api/core/rabbit/RabbitMqSender.java
+++ pay-api-core/src/main/java/com/pay/api/core/rabbit/RabbitMqSender.java
@@ -2,6 +2,8 @@ package com.pay.api.core.rabbit;
 
 import com.pay.api.client.constants.PayApiMessageQueueNames;
 import com.pay.api.client.dto.async.TradeCreateMessageDTO;
+import com.pay.asset.client.constants.PayAssetMessageQueueNames;
+import com.pay.asset.client.dto.async.TradeStatisticsMessageDTO;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,7 +33,17 @@ public class RabbitMqSender {
     /**
      * 发送交易风控或预警异步消息
      */
-    public void sendTradeRiskOrWarnMessage(){
+    public void sendTradeRiskOrWarnMessage() {
 
+    }
+
+
+    /**
+     * 发送交易统计异步消息
+     *
+     * @param tradeStatisticsMessageDTO 异步消息
+     */
+    public void sendTradeStatisticsMessage(TradeStatisticsMessageDTO tradeStatisticsMessageDTO) {
+        amqpTemplate.convertAndSend(PayAssetMessageQueueNames.QUEUE_TRADE_STATISTICS, tradeStatisticsMessageDTO);
     }
 }
