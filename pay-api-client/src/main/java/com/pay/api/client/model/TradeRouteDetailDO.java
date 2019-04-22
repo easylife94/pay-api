fbfc67f20cad9606ff73da82dfccf4d114354195
chenwei <chenwei@qq.com>
2019-04-22 22:38:20 +0800

    交易路由详情dao和model

---
+++ pay-api-client/src/main/java/com/pay/api/client/model/TradeRouteDetailDO.java
@@ -0,0 +1,33 @@
+package com.pay.api.client.model;
+
+import com.pay.common.client.model.AbstractBaseDO;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author chenwei
+ * @date 2019-04-22
+ */
+@Data
+public class TradeRouteDetailDO extends AbstractBaseDO {
+
+    private Long tradeRouteId;
+
+    private String serviceFeeType;
+
+    private BigDecimal serviceFee;
+
+    private BigDecimal tradeAmountMin;
+
+    private BigDecimal tradeAmountMax;
+
+    private String platformChannelProfitType;
+
+    private BigDecimal platformChannelProfit;
+
+    private String sysChannelProfitType;
+
+    private BigDecimal sysChannelProfit;
+
+}
---
+++ pay-api-core/src/main/java/com/pay/api/core/dao/TradeRouteDetailDao.java
@@ -0,0 +1,11 @@
+package com.pay.api.core.dao;
+
+import com.pay.api.client.model.TradeRouteDetailDO;
+import com.pay.common.core.dao.IBaseDao;
+
+/**
+ * @author chenwei
+ * @date 2019-04-22
+ */
+public interface TradeRouteDetailDao extends IBaseDao<TradeRouteDetailDO> {
+}
